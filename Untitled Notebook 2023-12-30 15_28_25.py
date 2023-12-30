2c893b070bd679f7be4a91c3ce1c6f6c273405a3
DataHater <DataHater>
2023-12-30 12:08:24 +0000

    dd

--- Untitled Notebook 2023-12-30 15_28_25.py
+++ Untitled Notebook 2023-12-30 15_28_25.py
@@ -31,15 +31,51 @@ stream_df = spark.readStream.schema(schema).json("/FileStore/tables/")
 
 # COMMAND ----------
 
+from pyspark.sql.types import StructType, StringType, ArrayType, IntegerType
 from pyspark.sql.functions import from_json, col, explode
 
-df3 = stream_df.withColumn( "testdata", from_json( col("jsondata"), schema_lake_segments) ) \
-         .drop("jsondata") \
-         .withColumn("segment_struct", explode(col('testdata'))) \
-         .drop("testdata") \
-         .withColumn("name", col('segment_struct.name')) \
-         .withColumn("id", col('segment_struct.id')) \
-         .drop("segment_struct")
+# Используйте правильную схему для чтения потокового DataFrame
+schema_stream = StructType().add("jsondata", StringType())
+schema_lake_segments = ArrayType(StructType().add("name", StringType()).add("id", IntegerType()))
+
+# Чтение потокового DataFrame
+stream_df = spark.readStream.schema(schema_stream).json("/FileStore/tables/")
+
+# Преобразование данных
+df_stream_transformed = stream_df.withColumn("testdata", from_json(col("jsondata"), schema_lake_segments)) \
+    .drop("jsondata") \
+    .withColumn("segment_struct", explode(col("testdata"))) \
+    .drop("testdata") \
+    .withColumn("name", col("segment_struct.name")) \
+    .withColumn("id", col("segment_struct.id")) \
+    .drop("segment_struct")
+
+# COMMAND ----------
+
+class LakeConfig:
+    def __init__(self, basePath):
+        self.basePath = basePath
+
+    def getCheckpointLocation(self, tableName):
+        return f"{self.basePath}/checkpoints/{tableName}"
+
+    def getTableRef(self, tableName):
+        return f"{self.basePath}/tables/{tableName}"
+
+lakeConfig = LakeConfig("dbfs:/user/hive/warehouse")
+
+def writeStreamTo(df, tableName):
+    tablePath = lakeConfig.getTableRef(tableName)
+    checkpointLocation = lakeConfig.getCheckpointLocation(tableName)
+
+    return df.writeStream.format("delta").outputMode("append") \
+        .trigger(availableNow=True) \
+        .option("mergeSchema", "true") \
+        .option("checkpointLocation", checkpointLocation) \
+        .start(tablePath)  # Изменено на start для указания пути
+
+# Использование функции с вашим потоковым DataFrame
+writeStreamTo(df_stream_transformed, "test_Table")
 
 # COMMAND ----------
 
